79a8fc345aa5043ff00256060c95bd6b4c8a6b93
Tiago Scolari <tscolari@gmail.com>
2013-08-18 23:15:23 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleAgenda::Application.config.secret_key_base = '03847a43ddf46286b7725207d2c598ef14644ac730a3489b62aa62a93ca8d671273acac120e33430e7517d05c1c86faf564e5019c5c1bb292066ee80058b2f98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoogleAgenda::Application.config.session_store :cookie_store, key: '_google_agenda_session'
