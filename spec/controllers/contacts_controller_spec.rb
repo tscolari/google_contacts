5e5f64eedfe5cdf61d49fab9d9e927a5dbf49827
Tiago Scolari <tscolari@gmail.com>
2013-08-19 02:12:15 +0100

    Fixing controller spec

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe ContactsController do
+  render_views
+  before(:each) do
+    sign_in FactoryGirl.create(:user)
+  end
 
-  describe "GET 'index'" do
+  let(:contacts) { 3.times.map { OpenStruct.new(name: '1', email: '2') } }
+
+  describe "get INDEX" do
     it "returns http success" do
+      ContactsAPI.stub_chain(:new, :contacts).and_return(contacts)
+      get :index
+      response.should be_success
     end
   end
 
